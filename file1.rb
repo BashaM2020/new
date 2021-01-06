665a0f043dea2c0fa39f02439e213c8c584c86d4
BashaM2020 <bashaninetyseven@gmail.com>
2021-01-05 21:09:42 -0500

    notes

---
+++ file1.rb
@@ -0,0 +1,75 @@
+# Tuesday, january, 5,2021
+# Solving problems :) with objects
+# programming models, combination of code blocks
+# (OOP) object-oriented programming, is a model to code, as seen in Ruby 
+# all codes have the code of C(functional programming model) 
+# Functional programming, very hard to understand for beginers
+# OOP is more relatable and easier for beginers to learn
+#
+#Everything in oop is defined by a Classes  and Objects 
+# 
+# Objects (is a defined entity), (charachteristics define it/ anything you can give its state(how it looks) and behavior/functions ex: a car, window, bank account, zebra)
+# Objects= 1)state/features/charachteristics 2)behavior/ function what is does
+# Objects have names. they are specific entities... animal is not a object, but dog is.
+# Advantages of objects is that it dosent care about the details of how the objects works. just wants to know what it can or cant do
+# behaviors of an object do the action- whichever action it is capable of
+#
+#Class
+#is a template from which we create an object. 
+#each object will be different and have the some different states and behaviors, but the same outline, generalized things, (
+#(the object itself defines its value/ its specific state and behavior)!
+#
+#ex: class=CAT
+#object1= this cat has 1)states=color 2)behaviors=eat, sleep, eat
+#object2= this other cat has another state and similar behaviors
+#class is an exisiting array from which we creat objects, 
+#there cant be objects without the class
+#
+#OOP is defined by classes/ blueprints and objects/instince
+#
+# How do we get objects to do things?
+# you direct them, if it can, it will, if not, ERROR
+#
+#
+# Data Encapsulations
+#
+#states are always private, and can't access it- only thru its behavior
+#behavior= method
+#state= data (private)
+#to get to a state u need to use a method
+#   1] 
+#enclassify objects= access data
+#ur code is capusled- and you need a code to encapsule it
+#your stated is defined by variables, u cant access those variables directly. u need a method to access those methods=
+#this tecnique keeps those states protected as found in OOP. this property= data encapsulation
+#you can modify charachteristics this is why its private, and prevent it from be exposed an effected by other codes
+# inheritance...parent class, child class.. common properties of its sub class with objects can inherit the parent class
+#
+#   2]
+#class inherits properties of another class
+#it helps in code resuse, and flexibility in code resuse
+#child properties defined in the parent class
+#media class= child class- album -movie, these properites dont need to be defined in each, so just define it in the parent class
+#
+#   3]
+#abstaction= process of hiding the process dedtails and showing the function to the user
+#
+#   4]
+#polymorphasin= object takes on other forms
+#done in a OOP model, method over-loading, methode over-ridding 
+#method-over-loading= the method name is the same but the num of arguments is different, this determins which method to be invoked and affect the objects
+#
+#method-over-riding= having the same method, with slight modifications
+#defing the same method of a parent class
+#writting the method in a parent class and re-writting it in the child, this changes the property from its parents
+#only possible in inheritance
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
